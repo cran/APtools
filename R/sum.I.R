9fad78ea7ca810204d606e682969d1f86113a071
Hengrui Cai <hengruicai@gmail.com>
2018-09-21 03:40:09 +0000

    version 6.8.8

--- R/sum.I.R
+++ R/sum.I.R
@@ -1,11 +1,11 @@
-sum.I <- function(yy,FUN,Yi,Vi=NULL){  	
+sum.I <- function(yy,FUN,Yi,Vi=NULL){
   if (FUN=="=") {
     out = rank(c(Yi,yy),ties.method="f")[-c(1:length(Yi))]-rank(c(yy,Yi),ties.method="f")[1:length(yy)]
     return(out)
   } else {
     if (FUN=="<"|FUN==">=") { yy <- -yy; Yi <- -Yi}
     # for each distinct ordered failure time t[j], number of Xi < t[j]
-    pos <- rank(c(yy,Yi),ties.method='f')[1:length(yy)]-rank(yy,ties.method='f')    
+    pos <- rank(c(yy,Yi),ties.method='f')[1:length(yy)]-rank(yy,ties.method='f')
     if (substring(FUN,2,2)=="=") pos <- length(Yi)-pos # number of Xi>= t[j]
     if (!is.null(Vi)) {
       ## if FUN contains '=', tmpind is the order of decending
@@ -15,5 +15,5 @@ sum.I <- function(yy,FUN,Yi,Vi=NULL){
       return(rbind(0,Vi)[pos+1,])
     } else return(pos)
   }
-  
 }
+
